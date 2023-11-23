0cf15b9ff213b6ecbcd6d921bcf59100d03e03d4
mizore-fu <mhxxname0782@gmail.com>
2023-11-23 13:50:09 +0900

    カウントのStateを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className="App">
+      <h1>Counter</h1>
+      <div>0</div>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
 }
 
 export default App;
